6caa813901acaf626ddb57db76d93d0483e6ae91
Brookelynn Constant <Brookelynn.constant@gmail.com>
2020-06-08 20:44:59 -0500

    updated list-jobs and list html files to show all jobs and selected in table form

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/ListController.java
@@ -28,10 +28,13 @@ public class ListController {
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
 
+
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
         tableChoices.put("coreCompetency", JobData.getAllCoreCompetency());
+//        tableChoices.put("allJobs", "All Jobs");
+
     }
 
     @RequestMapping(value = "")
